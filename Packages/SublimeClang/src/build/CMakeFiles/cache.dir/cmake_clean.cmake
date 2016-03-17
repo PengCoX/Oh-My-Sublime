20c995785163ba16c503c537b6406aaa3cf49c8c
PengCoX <pengc825@foxmail.com>
2016-03-17 22:13:07 +0800

    Oh-My-Sublime 初始化

---
+++ Packages/SublimeClang/src/build/CMakeFiles/cache.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/cache.dir/main.cpp.o"
+  "/home/pengc825/.config/sublime-text-3/Packages/SublimeClang/internals/libcache.pdb"
+  "/home/pengc825/.config/sublime-text-3/Packages/SublimeClang/internals/libcache.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/cache.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
